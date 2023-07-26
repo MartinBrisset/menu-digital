e487a86a01bdea829d84de1a0e4403983eb957e7
Martin Brisset <brissetmartin@gmail.com>
2023-07-26 16:55:18 -0300

    .

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,23 @@
+import { PrismaClient } from "@prisma/client"
 
-export default function Home() {
+export default function Home({ categorias }) {
+  
+  console.log(categorias)
+
   return (
     <h1>nextjs</h1>
   )
+}
+
+export const getServerSideProps = async () => {
+  const prisma = new PrismaClient()
+
+  const categorias = await prisma.categoria.findMany()
+
+  return {
+    props: {
+      categorias
+    }
+  }
+
 }
